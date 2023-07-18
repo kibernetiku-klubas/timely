69c01f74c6c3fd61cd484ff8890cace0799cf687
Marsietis <martynas.matijosius@gmail.com>
2023-07-18 18:04:13 +0300

    Added an add a meeting button, that redirects to add-meeting.blade.php view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+        <div class="flex items-center justify-center">
+        <a href="/add-meeting"><button class="btn btn-outline">Add a new meeting</button></a>
         </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/add-meeting', function (){
+        return view('add-meeting');
+    });
 });
 
 require __DIR__.'/auth.php';
