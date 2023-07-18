8e9a1d286b89d7feb26bd400951b1d45528b30d9
Marsietis <martynas.matijosius@gmail.com>
2023-07-18 19:16:59 +0300

    Renamed add-meeting.blade.php to add.blade.php and moved it to meetings folder

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/add-meeting', function (){
-        return view('add-meeting');
+        return view('meetings.add');
     });
 });
 
