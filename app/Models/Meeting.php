48b8441bf6143ae1494ccc72287bb9c4b0f40982
archlich03 <persona@minichungus.localdomain>
2023-07-18 16:28:44 +0300

    Pirminis variantas, tvarkantis DB table kūrimą

---
+++ app/Models/Meeting.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+class Meeting extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
+    }
+
+}
