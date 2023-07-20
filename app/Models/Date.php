151c6485670c6bc8afbc179529d2a40a7a3493b9
Marsietis <martynas.matijosius@gmail.com>
2023-07-20 15:08:20 +0300

    Added Date.php model with belongsTo Meeting classes meeting_id

---
+++ app/Models/Date.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Date extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dates';
+
+    protected $fillable = ['date_and_time', 'voted_by'];
+    public function meeting(): BelongsTo
+    {
+        return $this->belongsTo(Meeting::class, 'meeting_id', 'id');
+    }
+}
