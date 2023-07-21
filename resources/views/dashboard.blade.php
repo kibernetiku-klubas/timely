d51f0ccfd9ca9adf5a6778038d675ed51c97d50f
Marsietis <martynas.matijosius@gmail.com>
2023-07-21 13:00:31 +0300

    Created a new navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="flex items-center justify-center">
         <a href="/add-meeting"><button class="btn btn-outline">Add a new meeting</button></a>
         </div>
     </div>
-    
+
     <div class="card-container grid grid-cols-1 gap-4 place-items-center md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
     @foreach ($meetings as $meeting)
         <div class="card w-96 bg-primary-content shadow-x1 card-bordered">
