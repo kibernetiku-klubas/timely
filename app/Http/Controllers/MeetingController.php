5e7e26618298ad343664c58ebf3422a71bd0d4c7
Marsietis <martynas.matijosius@gmail.com>
2023-07-22 22:11:47 +0300

    Removed unused functions and unnecessary comments from MeetingController.php

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -10,25 +10,6 @@ use Illuminate\Http\Request;
 
 class MeetingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreMeeting $request): RedirectResponse
     {
         $validated = $request->validated();
@@ -47,35 +28,8 @@ class MeetingController extends Controller
         return redirect('/dashboard');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
         return view('meetings.meeting', ['meeting' => Meeting::findOrFail($id)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Meeting $meeting)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Meeting $meeting)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Meeting $meeting)
-    {
-        //
     }
 }
