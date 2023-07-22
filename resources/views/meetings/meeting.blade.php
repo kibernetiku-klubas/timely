5c86b52c983409aa75f254d4ae7c5d083bff5811
Marsietis <martynas.matijosius@gmail.com>
2023-07-22 23:12:59 +0300

    Temporary solution: Because meetings from a link should be viewable by unregistered users currently removed <x-app-layout> because it contained navbar, that only works for authorized users, because it displays username and other related things. Will have to create a guest navbar.

--- resources/views/meetings/meeting.blade.php
+++ resources/views/meetings/meeting.blade.php
@@ -1,16 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Meeting {{$meeting->title}}
-        </h2>
-    </x-slot>
-    Description:
-    {{$meeting->description}}<br>
-    Location:
-    {{$meeting->location}}<br>
-    Timezone: {{$meeting->timezone_offset}}<br>
-    Duration: {{$meeting->duration}}<br>
-    Meet times: {{$meeting->meet_times}}<br>
-    Meeting link: <a href="">https://domain.com/{{$meeting->id}}</a>
-</x-app-layout>
-
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Meeting {{$meeting->title}}
+    </h2>
+</x-slot>
+Description:
+{{$meeting->description}}<br>
+Location:
+{{$meeting->location}}<br>
+Timezone: {{$meeting->timezone_offset}}<br>
+Duration: {{$meeting->duration}}<br>
+Meet times: {{$meeting->meet_times}}<br>
+Meeting link: <a href="">https://domain.com/{{$meeting->id}}</a>
