25fdd6d8eed41ef7fd1cb3761d6d04172d4fc246
nitrogency <nitrog@protonmail.com>
2023-07-22 18:05:52 +0300

    Added error message to below 0 input.

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -35,8 +35,8 @@ class MeetingController extends Controller
             'description' => 'max:255',
             'location' => 'max:64',
             'timezone_offset' => 'integer|max:13',
-            'duration' => 'integer|max:32000',
-            'delete_after' => 'integer|max:32000'
+            'duration' => 'integer|max:32000|gt:0',
+            'delete_after' => 'integer|max:32000|gt:0'
         ]);
 
         // Additional validation for timezone_offset, duration, and delete_after current values are example values
