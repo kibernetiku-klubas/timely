7e202ca5123d4896458218d360958a87fd1db4cf
Marsietis <martynas.matijosius@gmail.com>
2023-07-25 17:21:14 +0300

    Added voting checkboxes
    Updated meeting.blade.php page with <x-app-layout> and created a temporary styling. Currently, this page is viewable to registered user only.

--- resources/views/meetings/meeting.blade.php
+++ resources/views/meetings/meeting.blade.php
@@ -1,3 +1,6 @@
+<x-app-layout>
+    <div class="flex flex-col sm:justify-center items-center py-8 sm:pt-0 bg-gray-100 text-black">
+        <div class="w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl mt-6 px-6 py-4 bg-white shadow-xl overflow-hidden rounded-lg">
 @include ('meetings.edit-times')
 <h2 class="text-xl text-gray-800 leading-tight">
     Meeting: {{$meeting->title}}
@@ -14,10 +17,27 @@ Updated at: {{$meeting->updated_at}}
 
 Dates and times for the meeting:
 
-<ul>
+<ul class="m-6">
 @foreach($dates as $date)
-    <li>
-    {{$date->date_and_time}}, votes: {{$date->voted_by}}
+    <li class="my-6 shadow-xl p-6 rounded-xl">
+    {{$date->date_and_time}},
+        <div class="container">
+        <div class="form-control">
+            <label class="label cursor-pointer">
+                <span class="label-text text-lg text-black">Vote on this date:</span>
+                <div class="shadow-xl">
+                <input type="checkbox" checked="checked" class="checkbox checkbox-lg checkbox-success" />
+                </div>
+            </label>
+        </div>
+        </div>
+        votes:
+        <div class="tooltip" data-tip="Names of people who voted">
+            Number of votes
+        </div>
     </li>
 @endforeach
 </ul>
+        </div>
+    </div>
+</x-app-layout>
