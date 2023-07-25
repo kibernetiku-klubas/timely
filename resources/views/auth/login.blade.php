704d093d4ea35ac5f264a87b1b2fd29ae042657f
Marsietis <martynas.matijosius@gmail.com>
2023-07-25 14:08:37 +0300

    Simplified login process and improved UI
    Removed the 'update profile information' form from the edit profile view, and the email address field from the login and registration forms, focusing on user's name for the purpose of simplicity and a cleaner user interface. Also, changes were implemented in the navigation layout, converting the logout process into a button action for a better user experience. The link for password recovery was removed from the login view, and replaced with a register prompt to emphasize user registration.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,10 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -33,11 +32,9 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Don\'t have an account yet?') }}
                 </a>
-            @endif
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
