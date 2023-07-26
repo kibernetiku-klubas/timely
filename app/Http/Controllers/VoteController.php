d99c4591ba4dec8db33f416dbf390302d0c9f000
Marsietis <martynas.matijosius@gmail.com>
2023-07-26 18:46:30 +0300

    Add VoteController to handle vote submission
    A new controller, 'VoteController', has been added to handle the storing action of meeting votes. It processes the voting request, creates and saves a 'Vote' instance if a valid vote is submitted. This change provides a dedicated means to handle vote submissions efficiently and ensures that votes data are correctly stored in the database.

---
+++ app/Http/Controllers/VoteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vote;
+use Illuminate\Http\Request;
+
+class VoteController extends Controller
+{
+    public function store(Request $request)
+    {
+        $meetingId = $request->input('meeting_id');
+        $votedBy = $request->input('voted_by');
+        $dateIds = $request->input('date_ids', []);
+        $votes = $request->input('votes', []);
+
+        foreach ($dateIds as $dateId) {
+            if (in_array($dateId, $votes)) {
+                Vote::create([
+                    'date_id' => $dateId,
+                    'voted_by' => $votedBy,
+                ]);
+            }
+        }
+
+        return redirect()->route('meeting.show', ['id' => $meetingId])->with('success', 'Votes saved successfully!');
+    }
+}
