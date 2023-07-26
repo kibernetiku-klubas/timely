c6245a2a93a7426249dd18df92c2a7e8ec654066
Marsietis <martynas.matijosius@gmail.com>
2023-07-26 21:56:21 +0300

    Refactor notification component for better usability
    Modified the notification blade component to display either success or error message. It previously displayed only success messages. This change enables more adaptable notifications across our app, improving user experience by providing more meaningful alerts with appropriate styling based on the type of message. Now notification also have green text for success and red for errors (Please provide feedback for this change, as it makes UI less clean, but UX better)

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,30 +1,29 @@
 <div>
     <div class="alert-container">
-        <div class="flex justify-center text-black bg-white px-4 py-3 rounded-lg shadow-xl relative max-w-lg mx-auto mt-6 p-6">
-            <strong class="font-bold">{{ session()->get('success') }}</strong>
+        <div class="flex justify-center {{ $type === 'success' ? 'text-green-600' : 'text-red-600' }} bg-white px-4 py-3 rounded-lg shadow-xl relative max-w-lg mx-auto mt-6 p-6">
+            <strong class="font-bold">{{ $message }}</strong>
         </div>
     </div>
 
-        <style>
-            .alert-container {
-                opacity: 1;
-                transition: opacity 0.3s ease-out;
-            }
+    <style>
+        .alert-container {
+            opacity: 1;
+            transition: opacity 0.3s ease-out;
+        }
 
-            .alert-container.fade-out {
-                opacity: 0;
-            }
+        .alert-container.fade-out {
+            opacity: 0;
+        }
+    </style>
 
-        </style>
+    <script>
+        setTimeout(function () {
+            let alertContainer = document.querySelector('.alert-container');
+            alertContainer.classList.add('fade-out');
 
-        <script>
             setTimeout(function () {
-                let alertContainer = document.querySelector('.alert-container');
-                alertContainer.classList.add('fade-out');
-
-                setTimeout(function () {
-                    alertContainer.style.display = 'none';
-                }, 400);
-            }, 2000);
-        </script>
+                alertContainer.style.display = 'none';
+            }, 400);
+        }, 2000);
+    </script>
 </div>
