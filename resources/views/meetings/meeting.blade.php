104e01f6123d0bc7339d691accc92313bcd6ed37
Marsietis <martynas.matijosius@gmail.com>
2023-07-26 21:57:57 +0300

    Improve UI feedback and error handling in meeting view
    The diff consists multiple improvements in the 'meetings' blade template. It adds a check to display error messages in a more informative way using a notification component if an error occurs during session. Code readability and consistency is improved with better formatting of the view.  Also added a conditional check in views to display 'No dates available for voting' message when 'dates' array is empty, providing a feedback to user in case dates are not available for voting. All checkboxes are unchecked by default. Close button was added.

--- resources/views/meetings/meeting.blade.php
+++ resources/views/meetings/meeting.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
+    @if (session()->has('error'))
+        <x-notification type="error" message="{{ session('error') }}" />
+    @endif
     @if(session()->has('success'))
-        <x-notification>
-        </x-notification>
+        <x-notification type="success" message="{{ session()->get('success') }}" />
     @endif
     <div class="flex flex-col sm:justify-center items-center py-8 sm:pt-0 bg-gray-100 text-black">
         <div
@@ -21,12 +23,18 @@
             Updated at: {{ $meeting->updated_at }}<br>
 
             <div class="text-right pr-16">
-                <button class="btn btn-outline border-none text-black text-xl shadow-black shadow-2xl hover:shadow-none" onclick="votesModal.showModal()">Vote on times</button>
+                <button class="btn btn-outline border-none text-black text-xl shadow-black shadow-2xl hover:shadow-none"
+                        onclick="votesModal.showModal()">Vote on times
+                </button>
             </div>
             <dialog id="votesModal" class="modal">
                 <form method="POST" action="{{ route('vote.store') }}" class="modal-box bg-white shadow-2xl">
                     <a class="btn btn-circle btn-outline mb-4" onclick="document.getElementById('votesModal').close();">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
                     </a>
                     @csrf
                     <input type="hidden" name="meeting_id" value="{{ $meeting->id }}">
@@ -34,29 +42,35 @@
                         save your votes</h3>
                     <p class="py-4 text-black">Note: Name will be visible to everyone viewing this meeting</p>
 
-                    <div>
-                        <x-input-label for="voted_by" :value="__('Your name')"/>
-                        <x-text-input id="voted_by" class="block mt-1 w-full text-black" type="text" name="voted_by"
-                                      :value="old('voted_by')" required autofocus/>
-                        <x-input-error :messages="$errors->get('voted_by')" class="mt-2 text-red-700"/>
-                    </div>
-                    @foreach($dates as $date)
-                        <input type="hidden" name="date_ids[]" value="{{ $date->id }}">
-                        <div class="form-control">
-                            <label class="label cursor-pointer">
-                                <span class="label-text text-lg text-black">Vote on: {{$date->date_and_time}}</span>
-                                <div class="shadow-xl">
-                                    <input type="checkbox" name="votes[]" value="{{ $date->id }}"
-                                           class="checkbox checkbox-lg checkbox-success">
-                                </div>
-                            </label>
+                    @if (count($dates) === 0)
+                        <p class="text-red-700 text-xl font-bold flex justify-center mt-4">NO DATES AVAILABLE FOR
+                            VOTING</p>
+                    @endif
+                    @if (count($dates) > 0)
+                        <div>
+                            <x-input-label for="voted_by" :value="__('Your name')"/>
+                            <x-text-input id="voted_by" class="block mt-1 w-full text-black" type="text" name="voted_by"
+                                          :value="old('voted_by')" required autofocus/>
+                            <x-input-error :messages="$errors->get('votes')" class="mt-2 text-red-700"/>
                         </div>
-                    @endforeach
-                    <div class="flex items-center justify-end mt-4">
-                        <x-primary-button type="submit" class="ml-4">
-                            {{ __('Save votes') }}
-                        </x-primary-button>
-                    </div>
+                        @foreach($dates as $date)
+                            <input type="hidden" name="date_ids[]" value="{{ $date->id }}">
+                            <div class="form-control">
+                                <label class="label cursor-pointer">
+                                    <span class="label-text text-lg text-black">Vote on: {{$date->date_and_time}}</span>
+                                    <div class="shadow-xl">
+                                        <input type="checkbox" name="votes[]" value="{{ $date->id }}"
+                                               class="checkbox checkbox-lg checkbox-success">
+                                    </div>
+                                </label>
+                            </div>
+                        @endforeach
+                        <div class="flex items-center justify-end mt-4">
+                            <x-primary-button type="submit" class="ml-4">
+                                {{ __('Save votes') }}
+                            </x-primary-button>
+                        </div>
+                    @endif
                 </form>
             </dialog>
 
