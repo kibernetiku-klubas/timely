06775506391086f4918f5b425c69b4ca636af4f6
Marsietis <martynas.matijosius@gmail.com>
2023-07-27 20:26:48 +0300

    Update 'Name' label to 'Username' in auth views
    In accordance with the changed user data field from 'Name' to 'Username', the label for the corresponding input field in both 'login.blade.php' and 'register.blade.php' views has been updated. This will ensure users understand what information is required in the input field.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         @csrf
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Username')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
