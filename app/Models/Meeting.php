431e6bae5222ea89cfb8990a6d3fd0fc91b88a26
Marsietis <martynas.matijosius@gmail.com>
2023-07-27 15:10:49 +0300

    Add HasManyThrough relationship to Meeting model
    A HasManyThrough relationship was added to the Meeting model to allow the retrieval of all votes associated with a meeting through the Date model. This change better organizes the relationships between our data and efficiently accesses related information.

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Ramsey\Uuid\Uuid;
 
 class Meeting extends Model
@@ -42,5 +43,10 @@ class Meeting extends Model
     public function dates(): HasMany
     {
         return $this->hasMany(Date::class);
+    }
+
+    public function votes(): HasManyThrough
+    {
+        return $this->hasManyThrough(Vote::class, Date::class);
     }
 }
