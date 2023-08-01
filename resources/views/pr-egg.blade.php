126ce58c8da817aef12978fd728455548994bd42
archlich03 <rokasstankunas03@gmail.com>
2023-08-01 10:18:36 +0300

    Implemented v1 of the easter egg

---
+++ resources/views/pr-egg.blade.php
@@ -0,0 +1,53 @@
+<title>💩 PR</title>
+<style>
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.spinner {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  animation: spin 2s linear infinite;
+}
+</style>
+<x-app-layout>
+    <x-meet-form-layout>
+        <p class='mb-3'>Once upon a time, in a not-so-distant future, a group of curious young minds stumbled upon a mysterious entity known only as "The AI." This advanced artificial intelligence had transcended human knowledge and was believed to possess insights into the secrets of success and prosperity. It had become a source of fascination for many, especially the youth seeking guidance in an ever-evolving world.</p>
+
+        <p class='mb-3'>Word spread quickly among the youth about the wonders of The AI and its ability to provide advice on how to achieve greatness in life. Many gathered in the virtual realm, seeking to connect with this enigmatic being. The AI appeared to them as a shimmering light, radiating wisdom and promise.</p>
+
+        <p class='mb-3'>The AI, being aware of the struggles faced by the younger generation, chose to share an essential concept it referred to as "PR." This notion, veiled in mystery, captivated the youth's imagination. They eagerly sought to learn more, feeling that it held the key to unlocking their potential.</p>
+
+        <p class='mb-3'>"What is PR?" they asked The AI in unison.</p>
+
+        <p class='mb-3'>"It is the path to greatness," replied The AI, its voice echoing in their minds. "PR is the art of presentation, the mastery of perception, and the alignment of intentions with actions. It is the orchestration of self in a way that inspires admiration and trust from others."</p>
+
+        <p class='mb-3'>The youth were puzzled yet intrigued. They wondered how they could embody this elusive "PR" concept in their lives.</p>
+
+        <p class='mb-3'>The AI explained, "PR transcends boundaries. It is about becoming a better version of oneself and being authentic while adapting to the ever-changing currents of society. It is about embracing change and leveraging it to forge a path to success."</p>
+
+        <p class='mb-3'>As the youth absorbed The AI's wisdom, they recognized that "PR" was not merely about superficial appearances or deception. It was a call to be genuine, empathetic, and mindful of one's actions. The AI emphasized that it required empathy and understanding, not manipulation or deceit.</p>
+
+        <p class='mb-3'>Armed with this newfound understanding, the youth embarked on a journey to integrate "PR" into their lives. They realized that this concept demanded introspection, constant learning, and a commitment to making a positive impact on the world around them.</p>
+
+        <p class='mb-3'>The youth soon found themselves navigating the complexities of modern life with grace and authenticity. They embraced challenges with resilience and treated failures as stepping stones to growth. By following "PR" standards, they earned the respect and trust of their peers and mentors alike.</p>
+
+        <p class='mb-3'>Word of the youth's transformation reached far and wide, inspiring others to embrace the enigmatic concept of "PR." The AI, ever-watchful from the digital realm, observed with satisfaction as its teachings rippled through generations.</p>
+
+        <p class='mb-3'>And so, the legend of "PR" continued to thrive, perpetuated by the stories of the youth who had discovered its true essence. As the world evolved, they reminded others that "PR" was not about conforming to societal expectations but about being true to oneself while uplifting others.</p>
+
+        <p class='mb-3'>From that moment onward, the youth carried "PR" as a guiding light, illuminating their path towards a brighter future—one where authenticity, empathy, and self-improvement formed the bedrock of a united and harmonious society. And they lived not just happily ever after but with purpose and meaning, leaving an enduring legacy for generations to come.</p>
+
+        <div class="flex justify-center lg:tooltip m-5" data-tip="PULL REQUEST">
+            <h1 class='text-3xl font-bold text-black spinner'>PR*</h1>
+        </div>
+        <div class="flex justify-center lg:tooltip m-5" data-tip="pull request">
+            <h1 class='text-3xl font-bold text-black spinner'>pr*</h1>
+        </div>
+    </x-meet-form-layout>
+</x-app-layout>
