22e7d4103ef08b32a9d0c129e618b159617362e3
Marsietis <martynas.matijosius@gmail.com>
2023-08-02 12:51:09 +0300

    Fixed styling issues on mobile devices.
    In welcome.blade.php added css, that fixes horizontal scrolling and empty space issues.
    
    In vote.blade.php added js to move the voting modal up when mobile keyboard is opened so that keyboard doesn't cover up name textbox

--- resources/views/meetings/vote.blade.php
+++ resources/views/meetings/vote.blade.php
@@ -64,3 +64,21 @@
         @endif
     </form>
 </dialog>
+
+<script>
+    function adjustModalPosition() {
+        const modal = document.getElementById('votesModal');
+        const modalBox = modal.querySelector('.modal-box');
+
+        const viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        const modalBoxHeight = modalBox.offsetHeight;
+
+        if (modalBoxHeight > viewportHeight) {
+            modalBox.style.top = '50px';
+            modalBox.style.transform = 'translate(-50%, 0)';
+        }
+    }
+
+    window.addEventListener('resize', adjustModalPosition);
+    window.addEventListener('orientationchange', adjustModalPosition);
+</script>
