c2f7ec44f0f965d29fb3bccdebfd38980a4d71e1
nitrogency <nitrog@protonmail.com>
2023-08-10 21:14:55 +0300

    Added edit-votes english localization

---
+++ lang/lt/edit-votes.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Edit-votes page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'clickhere' => 'Click here to edit votes',
+    'select' => 'Select a time to edit votes:',
+    'date' => 'Date and Time:',
+    'editname' => 'EDIT VOTE NAME',
+    'save' => 'Save',
+    'delete' => 'Delete vote',
+    'novotes' => 'No votes on this date',
+    'nodates' => 'No dates exist for this meeting',
+    
+];
