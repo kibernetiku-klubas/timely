02280b2bfd58e1681c8c9285dee7401ea3f2987c
nitrogency <nitrog@protonmail.com>
2023-08-10 20:39:02 +0300

    Added meeting-cards english localization

---
+++ lang/lt/meeting-cards.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Meeting-cards Page Language lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'mostvoted' => 'Most voted',
+    'delete' => 'Delete',
+    'confirm' => 'Confirm Deletion.',
+    'confirm_message' => 'Are you sure you want to delete this date?',
+    'cancel' => 'Cancel',
+    'confirmdelete' => 'Confirm Delete',
+    'export' => 'Export to calendar',
+    'votes' => 'Votes:',
+    'taken' => 'TAKEN.',
+    'free' => 'FREE.',
+    'seewho' => 'CLICK TO SEE WHO VOTED',
+    'novotes' => 'NO VOTES ON THIS DATE',
+    'whovoted' => 'PEOPLE WHO VOTED:',
+    
+    
+    
+];
