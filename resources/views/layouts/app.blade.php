780c31d25179782e05594b171dbc2cb5669136a5
Marsietis <martynas.matijosius@gmail.com>
2023-08-10 22:21:59 +0300

    "Update footer with additional information and links
    The footer in both welcome and app pages were updated to improve user navigation and provide additional resources. Notably, email support link and links to Privacy Policy and Terms of Service were added. Furthermore, styling and organization of footer elements was modified to enhance aesthetic and readability."

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased">
+<div class="min-h-screen bg-gray-100">
+    @include('layouts.navigation')
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
+<footer class="footer footer-center p-4 bg-white text-black">
+    <div>
+        <div class="flex items-center space-x-2">
+            <div class="text-black text-lg font-bold p-2">Timely</div>
+            <div class="text-sm p-2">v1.1.1-beta</div>
+            <div class="p-2">&copy; 2023 Kibernetiku klubas</div>
+            <div><a href="mailto:support@timely.lt" class="p-2 rounded-lg btn-ghost">support@timely.lt</a></div>
         </div>
-        <footer class="footer footer-center p-4 bg-white text-black">
-            <div>
-                <p>Timely, a meeting scheduling and time managing web app
-                    Copyright (C) 2023 Kibernetiku klubas | v1.1.1-beta</p>
-            </div>
-        </footer>
-    </body>
+        <div class="flex items-center space-x-2 mt-2">
+            <div>| <a href="/privacy-policy" target="_blank" class="p-2 rounded-lg btn-ghost">Privacy Policy</a> |</div>
+            <div><a href="/terms-of-service" target="_blank" class="p-2 rounded-lg btn-ghost">Terms of Service</a> |</div>
+            <div><a href="https://github.com/kibernetiku-klubas/timely/blob/main/SECURITY.md" target="_blank" class="p-2 rounded-lg btn-ghost">Report a Vulnerability</a> |</div>
+        </div>
+    </div>
+</footer>
+
+</body>
 </html>
