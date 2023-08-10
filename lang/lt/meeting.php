2d21e4061a3bf2eff26a9e7574fc93490e858f47
nitrogency <nitrog@protonmail.com>
2023-08-10 20:08:09 +0300

    Added meeting english localization

---
+++ lang/lt/meeting.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Meeting Page Language lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'meeting' => 'MEETING.',
+    'dates' => 'DATES AND TIMES FOR THE MEETING:',
+    '1v1' => 'Note: This meeting is 1 on 1, so only one vote per date is allowed.',
+    'timesarein' => 'All times are in',
+    'timezone' => 'timezone',
+    'nodates' => 'NO DATES YET',
+    'adddates' => 'Add dates by using the date selector above',
+    
+];
