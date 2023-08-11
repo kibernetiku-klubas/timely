66eecc0f69e821185f1006a5dee21511393c6282
Benas <129758495+nitrogency@users.noreply.github.com>
2023-08-11 10:26:18 +0300

    Added localization to ToS

--- lang/lt/register.php
+++ lang/lt/register.php
@@ -13,10 +13,14 @@ return [
     |
     */
 
+    'tab' => 'Register',
     'name' => 'USERNAME',
     'passwd' => 'PASSWORD',
     'confirm' => 'CONFIRM PASSWORD',
     'registered' => 'Already registered?',
     'register' => 'Register',
-    
+    'agree' => 'I agree to the',
+    'tos' => 'Terms of Service',
+    'and' => 'and',
+    'policy' => 'Privacy Policy',
 ];
