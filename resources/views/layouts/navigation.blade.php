184fe5fe82477896deec6e35763525c2a4141128
nitrogency <nitrog@protonmail.com>
2023-08-11 09:37:19 +0300

    Added navigation english localization

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="flex justify-center sm:justify-end">
             <a href="/add-meeting" class="font-bold btn btn-ghost mx-4 sm:mx-6 text-xl flex items-center">
-                CREATE A MEETING
+                {{ __('navigation.create') }}
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                 </svg>
@@ -21,10 +21,10 @@
 
                     <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-200 rounded-box w-40 sm:w-52 text-black">
                         <li>
-                            <a href="{{ route('profile.edit') }}">Profile</a>
+                            <a href="{{ route('profile.edit') }}">{{ __('navigation.profile') }}</a>
                         </li>
                         <li>
-                            <button type="submit">{{ __('Log Out') }}</button>
+                            <button type="submit">{{ __('navigation.logout') }}</button>
                         </li>
                     </ul>
                 </form>
