bcb458e7e3fb48a0cf07477d28e1eb4789960e13
nitrogency <nitrog@protonmail.com>
2023-08-12 19:40:47 +0300

    Added LT/EN finalize-date translation

---
+++ lang/lt/finalize-date.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Finalize-date page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'finalize' => 'PASKIRTI',
+    'time' => 'LAIKĄ.',
+    'mark' => 'Šis veiksmas pažymės šį laiką kaip galutinį visiems dalyviams, tačiau Jūs vis
+    dar turėsite galimybę jį pakeisti vėliau.',
+    'select' => 'Pasirinkite laiką, kurį norite paskirti:',
+    'votes' => 'Balsai:',
+    'cancel' => 'Atšaukti',
+    'confirm' => 'Patvirtinti',
+
+];
--- resources/views/meetings/finalize-date.blade.php
+++ resources/views/meetings/finalize-date.blade.php
@@ -1,17 +1,15 @@
 <x-app-layout>
     <div class="flex flex-col sm:justify-center items-center mt-8">
         <div>
-            <div class="fill-current text-black text-4xl font-bold">FINALIZE</div>
-            <div class="fill-current text-black text-4xl font-bold">A TIME.</div>
+            <div class="fill-current text-black text-4xl font-bold">{{ __('finalize-date.finalize') }}</div>
+            <div class="fill-current text-black text-4xl font-bold">{{ __('finalize-date.time') }}</div>
         </div>
     </div>
     <x-meet-form-layout>
         <form method="POST" action="{{ route('meetings.finalize-date', $meeting->id) }}">
             @csrf
-            <p class='text-black mt-2 font-medium'>This will mark a time as final for everyone. You can still
-                change this later.
-            </p>
-            <p class='text-black font-bold text-xl uppercase mt-4'>Select a time to finalize:</p>
+            <p class='text-black mt-2 font-medium'>{{ __('finalize-date.mark') }}</p>
+            <p class='text-black font-bold text-xl uppercase mt-4'>{{ __('finalize-date.select') }}</p>
 
             @php
                 $sortedDates = $meeting->dates->sortByDesc(function ($date) {
@@ -24,16 +22,16 @@
                 <label class="flex items-center mb-2">
                     <input type="radio" name="selected_date" value="{{ $dateOption->id }}" class="mr-2 radio checked:bg-purple-500 radio-lg shadow-xl">
                     <div class="text-black">{{ $dateOption->date_and_time }}</div>
-                    <div class="ml-2 text-black uppercase">Votes: {{ $dateOption->votes->count() }}</div>
+                    <div class="ml-2 text-black uppercase">{{ __('finalize-date.votes') }} {{ $dateOption->votes->count() }}</div>
                 </label>
             @endforeach
             </div>
 
             <div class="mt-6">
                 <a href="{{ route('meeting.show', $meeting->id) }}">
-                    <x-secondary-button>Cancel</x-secondary-button>
+                    <x-secondary-button>{{ __('finalize-date.cancel') }}</x-secondary-button>
                 </a>
-                <x-primary-button type="submit" class="ml-2">Confirm</x-primary-button>
+                <x-primary-button type="submit" class="ml-2">{{ __('finalize-date.confirm') }}</x-primary-button>
             </div>
         </form>
     </x-meet-form-layout>
