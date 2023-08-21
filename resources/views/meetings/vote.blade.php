9c8d01564a42e8b4853021b7dc115440ae58daa4
Marsietis <martynas.matijosius@gmail.com>
2023-08-21 15:57:33 +0300

    Move "votesModal" script into the view file
    This is to stop server error because the js file was not loaded

--- resources/views/meetings/vote.blade.php
+++ resources/views/meetings/vote.blade.php
@@ -1,5 +1,21 @@
-@vite(['resources/js/votes-modal.js'])
+<script>
+    function adjustModalPosition() {
+        const modal = document.getElementById('votesModal');
+        const modalBox = modal.querySelector('.modal-box');
 
+        const viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        const modalBoxHeight = modalBox.offsetHeight;
+
+        if (modalBoxHeight > viewportHeight) {
+            modalBox.style.top = '50px';
+            modalBox.style.transform = 'translate(-50%, 0)';
+        }
+    }
+
+    window.addEventListener('resize', adjustModalPosition);
+    window.addEventListener('orientationchange', adjustModalPosition);
+
+</script>
 @php
     $isDisabledBtnClass = $hasVoted ? 'shadow-2xl' : 'shadow-lg hover:shadow-none';
     $btnClickHandler = $hasVoted ? '' : 'onclick="votesModal.showModal()"';
