da4f372e9a4e63fad79f8bf4a4651dc0a2336a4c
marsietis <martynas.matijosius@gmail.com>
2024-02-03 21:31:27 +0200

    Updated the version number for release v1.3.0 and changed the year on copyright to 2024.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,8 +2,8 @@
     <div>
         <div class="flex items-center space-x-2">
             <div class="text-black text-lg font-bold p-2">Timely</div>
-            <div class="text-sm p-2">v1.2.1-beta</div>
-            <div class="p-2">&copy; 2023 Kibernetiku klubas</div>
+            <div class="text-sm p-2">v1.3.0-beta</div>
+            <div class="p-2">&copy; 2024 Kibernetiku klubas</div>
             <div><a href="mailto:support@timely.lt" class="p-2 rounded-lg btn-ghost">support@timely.lt</a></div>
         </div>
         <div class="flex items-center space-x-2 mt-2">
