9f0160d192f3791411ce75c083f2cc617a766e05
marsietis <martynas.matijosius@gmail.com>
2024-02-03 16:23:57 +0200

    Added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="flex flex-col items-center">
+        <div class="mt-36 mx-20">
+            <div class="fill-current text-black text-6xl font-bold uppercase">{{ __('404') }}</div>
+            <div class="fill-current text-black text-6xl font-bold uppercase mb-8">{{ __('Page Not Found.') }}</div>
+        </div>
+    </div>
+    <div class="flex flex-col items-center">
+        <h2 class="text-2xl text-black font-bold uppercase mx-20">{{ __('Sorry, the page you are looking for might be in another castle.') }}</h2>
+    </div>
+</x-app-layout>
