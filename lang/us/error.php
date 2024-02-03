85f5f4c400341fd6169023a3849f3488568704e7
marsietis <martynas.matijosius@gmail.com>
2024-02-03 16:48:01 +0200

    Added custom 500 page and added translations.

---
+++ lang/us/error.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Error Page Language lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'notfound' => 'Page Not Found.',
+    '404description' => 'Sorry, the page you are looking for might be in another castle.',
+    'servererror' => 'Server Error.',
+    '500description1' => 'Our server is currently in a meeting with some stubborn lines of code.',
+    '500description2' => 'It will be back to help you schedule your meetings ASAP!'
+];
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,10 +2,10 @@
     <div class="flex flex-col items-center">
         <div class="mt-36 mx-20">
             <div class="fill-current text-black text-6xl font-bold uppercase">{{ __('404') }}</div>
-            <div class="fill-current text-black text-6xl font-bold uppercase mb-8">{{ __('Page Not Found.') }}</div>
+            <div class="fill-current text-black text-6xl font-bold uppercase mb-8">{{ __('error.notfound') }}</div>
         </div>
     </div>
     <div class="flex flex-col items-center">
-        <h2 class="text-2xl text-black font-bold uppercase mx-20">{{ __('Sorry, the page you are looking for might be in another castle.') }}</h2>
+        <h2 class="text-2xl text-black font-bold uppercase mx-20">{{ __('error.404description') }}</h2>
     </div>
 </x-app-layout>
