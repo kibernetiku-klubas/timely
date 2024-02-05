29107dded9cd8c5c4151cfad28ddf58a2ac2bc2f
marsietis <martynas.matijosius@gmail.com>
2024-02-05 21:51:44 +0200

    Added comments

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -8,9 +8,14 @@ use Illuminate\Support\Facades\Session;
 
 class LanguageController extends Controller
 {
+    /**
+     * Switch the application language based on the provided language code.
+     */
     public function switchLang($lang)
     {
+        // Check if the provided language code exists in the configured languages.
         if (array_key_exists($lang, Config::get('languages'))) {
+            // Set the application locale in the session to the selected language.
             Session::put('applocale', $lang);
         }
 
