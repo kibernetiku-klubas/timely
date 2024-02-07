6f7a4096195beb77497815ac26cc709eb090b306
marsietis <martynas.matijosius@gmail.com>
2024-02-07 22:33:15 +0200

    Added comment for MeetingAuthorizationTrait.php isUserMeetingCreator function

--- app/Traits/MeetingAuthorizationTrait.php
+++ app/Traits/MeetingAuthorizationTrait.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Auth;
 
 trait MeetingAuthorizationTrait
 {
+    /*
+     * Checks if Authorized user is meeting's creator by passing meeting's ID
+     */
     private function isUserMeetingCreator(string $meetingId): bool
     {
         return Meeting::where('id', $meetingId)
